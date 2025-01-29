ddb7239e3cc07378e90bf49e4bea02232b57a221
Lautaro Moreno <lemoreno2002@gmail.com>
2025-01-29 20:22:12 -0300

    algunas modificaciones

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -15,9 +15,14 @@ const appendProduct = (product, res) => {
   newProduct.save();
 }
 
+// Retorna todos los productos
 router.get('/', async (req, res) => {
   try {
-    res.status(200).json(await Product.find());
+    const products = await Product.find();
+    if (products.length === 0) 
+      res.status(201).json({ message: 'No hay productos cargados', products });
+    else 
+      res.status(200).json({ message: `Cantidad de productos: ${products.length}.`, products });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -90,42 +95,94 @@ router.post('/', (req, res) => {
   const { products } = req.body;
   if (!products) {
     appendProduct(req.body, res);
-    res.status(201).json(req.body);
+    res.status(201).json({ message: 'Producto creado correctamente.', product: req.body });
   } else {
     try {
       products.forEach(product => {
         appendProduct(product, res);
       });
-    res.status(201).json(products);
+    res.status(201).json({ message: 'Productos creados correctamente.', products });
  } catch (error) {
     res.status(400).json({ message: error.message });
   }
 }
 });
 
-router.get('/:id', getProduct, async (req, res) => {
-  res.json(res.product)
-});
-
-router.delete('/:id', getProduct, async (req, res) => {
-  try {
-    await res.product.deleteOne({
-      _id: res.product._id
-    })
-    res.json({ message: 'Producto eliminado' });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
 
 // Elimina todo de forma global
 router.delete('/', async (req, res) => {
   try {
     await Product.deleteMany({});
-    res.json({ message: 'Productos eliminados' });
+    res.json({ message: 'Productos eliminados correctamente.' });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 })
 
+// Retorna un producto
+router.get('/:id', getProduct, async (req, res) => {
+  res.json(res.product);
+});
+
+// Elimina un producto
+router.delete('/:id', getProduct, async (req, res) => {
+  try {
+    await res.product.deleteOne({
+      _id: res.product._id
+    })
+    res.json({ message: 'Producto eliminado correctamente.' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Actualiza un producto por completo
+router.put('/:id', getProduct, async (req, res) => {
+  const { brand, model, release_year, price, osname, osversion, dimensions, weight, specifications, connectivity, colors } = req.body;
+  if (!brand || !model || !release_year || !price || !osname || !osversion || !dimensions || !weight || !specifications || !connectivity || !colors) {
+    return res.status(400).json({ message: 'Debe enviar todos los campos para actualizar.' });
+  }
+  res.product.brand = brand || res.product.brand;
+  res.product.model = model || res.product.model;
+  res.product.release_year = release_year || res.product.release_year;
+  res.product.price = price || res.product.price;
+  res.product.osname = osname || res.product.osname;
+  res.product.osversion = osversion || res.product.osversion;
+  res.product.dimensions = dimensions || res.product.dimensions;
+  res.product.weight = weight || res.product.weight;
+  res.product.specifications = specifications || res.product.specifications;
+  res.product.connectivity = connectivity || res.product.connectivity;
+  res.product.colors = colors || res.product.colors;
+  try {
+    await res.product.save();
+    res.json({ message: 'Producto actualizado completamente.', product: res.product });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// Actualiza una parte de un producto
+router.patch('/:id', getProduct, async (req, res) => {
+  const { brand, model, release_year, price, osname, osversion, dimensions, weight, specifications, connectivity, colors } = req.body;
+  if (!brand && !model && !release_year && !price && !osname && !osversion && !dimensions && !weight && !specifications && !connectivity && !colors) {
+    return res.status(400).json({ message: 'Debe enviar al menos un campo para actualizar.' });
+  }
+  res.product.brand = brand || res.product.brand;
+  res.product.model = model || res.product.model; 
+  res.product.release_year = release_year || res.product.release_year;
+  res.product.price = price || res.product.price;
+  res.product.osname = osname || res.product.osname;
+  res.product.osversion = osversion || res.product.osversion;
+  res.product.dimensions = dimensions || res.product.dimensions;
+  res.product.weight = weight || res.product.weight;
+  res.product.specifications = specifications || res.product.specifications;
+  res.product.connectivity = connectivity || res.product.connectivity;
+  res.product.colors = colors || res.product.colors;
+  try {
+    await res.product.save();
+    res.json({ message: 'Producto actualizado parcialmente.', product: res.product });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
 module.exports = router;
