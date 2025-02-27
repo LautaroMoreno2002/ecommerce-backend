89218755d3ead3c880b63a866d5a8ae5a20c5f26
Lautaro Moreno <lemoreno2002@gmail.com>
2025-02-26 23:15:25 -0300

    Idea de autenticación con JWT

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -1,28 +1,72 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Product = require('../models/product.model');
-const getProduct = require('../middlewares/getProduct');
+const Product = require("../models/product.model");
+const getProduct = require("../middlewares/getProduct");
 
 const appendProduct = (product, res) => {
-  const { brand, model, release_year, price, osname, osversion, dimensions, weight, specifications, connectivity, colors, imgSrc } = product;
+  const {
+    brand,
+    model,
+    release_year,
+    price,
+    osname,
+    osversion,
+    dimensions,
+    weight,
+    specifications,
+    connectivity,
+    colors,
+    imgSrc,
+  } = product;
 
-  if (!brand || !model || !release_year || !price || !osname || !osversion || !dimensions || !weight || !specifications || !connectivity || !colors || !imgSrc) {
-    return res.status(400).json({ message: 'Los campos deben estar completos.' });
+  if (
+    !brand ||
+    !model ||
+    !release_year ||
+    !price ||
+    !osname ||
+    !osversion ||
+    !dimensions ||
+    !weight ||
+    !specifications ||
+    !connectivity ||
+    !colors ||
+    !imgSrc
+  ) {
+    return res
+      .status(400)
+      .json({ message: "Los campos deben estar completos." });
   }
   const newProduct = new Product({
-    brand, model, release_year, price, osname, osversion, dimensions, weight, specifications, connectivity, colors, imgSrc
+    brand,
+    model,
+    release_year,
+    price,
+    osname,
+    osversion,
+    dimensions,
+    weight,
+    specifications,
+    connectivity,
+    colors,
+    imgSrc,
   });
   newProduct.save();
-}
+};
 
 // Retorna todos los productos
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const products = await Product.find();
-    if (products.length === 0) 
-      res.status(201).json({ message: 'No hay productos cargados', products });
-    else 
-      res.status(200).json({ message: `Cantidad de productos: ${products.length}.`, products });
+    if (products.length === 0)
+      res.status(201).json({ message: "No hay productos cargados", products });
+    else
+      res
+        .status(200)
+        .json({
+          message: `Cantidad de productos: ${products.length}.`,
+          products,
+        });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -30,7 +74,7 @@ router.get('/', async (req, res) => {
 
 // router.post('/', (req, res) => {
 //   console.log(req.body);
-  
+
 //   const { brand, model, release_year, price, osname, osversion, dimensions, weight, specifications, connectivity, colors } = req?.body;
 
 //   if (!brand || !model || !release_year || !price || !osname || !osversion || !dimensions || !weight || !specifications || !connectivity || !colors) {
@@ -64,7 +108,7 @@ router.get('/', async (req, res) => {
 //   try {
 //     for (product of products) {
 //       console.log(product);
-      
+
 //       const { brand, model, release_year, price, osname, osversion, dimensions, weight, specifications, connectivity, colors } = product;
 //       if (!brand || !model || !release_year || !price || !osname || !osversion || !dimensions || !weight || !specifications || !connectivity || !colors) {
 //         return res.status(400).json({ message: 'Los campos deben estar completos en cada producto.' });
@@ -82,7 +126,7 @@ router.get('/', async (req, res) => {
 //       connectivity,
 //       colors
 //     });
-//     newProduct.save();  
+//     newProduct.save();
 //   }
 //   res.status(201).json(products);
 //  } catch (error) {
@@ -91,56 +135,87 @@ router.get('/', async (req, res) => {
 //   }
 // )
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   const { products } = req.body;
   if (!products) {
     appendProduct(req.body, res);
-    res.status(201).json({ message: 'Producto creado correctamente.', product: req.body });
+    res
+      .status(201)
+      .json({ message: "Producto creado correctamente.", product: req.body });
   } else {
     try {
-      products.forEach(product => {
+      products.forEach((product) => {
         appendProduct(product, res);
       });
-    res.status(201).json({ message: 'Productos creados correctamente.', products });
- } catch (error) {
-    res.status(400).json({ message: error.message });
+      res
+        .status(201)
+        .json({ message: "Productos creados correctamente.", products });
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
   }
-}
 });
 
-
 // Elimina todo de forma global
-router.delete('/', async (req, res) => {
+router.delete("/", async (req, res) => {
   try {
     await Product.deleteMany({});
-    res.json({ message: 'Productos eliminados correctamente.' });
+    res.json({ message: "Productos eliminados correctamente." });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
-})
+});
 
 // Retorna un producto
-router.get('/:id', getProduct, async (req, res) => {
+router.get("/:id", getProduct, async (req, res) => {
   res.json(res.product);
 });
 
 // Elimina un producto
-router.delete('/:id', getProduct, async (req, res) => {
+router.delete("/:id", getProduct, async (req, res) => {
   try {
     await res.product.deleteOne({
-      _id: res.product._id
-    })
-    res.json({ message: 'Producto eliminado correctamente.' });
+      _id: res.product._id,
+    });
+    res.json({ message: "Producto eliminado correctamente." });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
 
 // Actualiza un producto por completo
-router.put('/:id', getProduct, async (req, res) => {
-  const { brand, model, release_year, price, osname, osversion, dimensions, weight, specifications, connectivity, colors, imgSrc } = req.body;
-  if (!brand || !model || !release_year || !price || !osname || !osversion || !dimensions || !weight || !specifications || !connectivity || !colors || !imgSrc) {
-    return res.status(400).json({ message: 'Debe enviar todos los campos para actualizar.' });
+router.put("/:id", getProduct, async (req, res) => {
+  const {
+    brand,
+    model,
+    release_year,
+    price,
+    osname,
+    osversion,
+    dimensions,
+    weight,
+    specifications,
+    connectivity,
+    colors,
+    imgSrc,
+  } = req.body;
+  if (
+    !brand ||
+    !model ||
+    !release_year ||
+    !price ||
+    !osname ||
+    !osversion ||
+    !dimensions ||
+    !weight ||
+    !specifications ||
+    !connectivity ||
+    !colors ||
+    !imgSrc
+  ) {
+    return res
+      .status(400)
+      .json({ message: "Debe enviar todos los campos para actualizar." });
   }
   res.product.brand = brand || res.product.brand;
   res.product.model = model || res.product.model;
@@ -155,20 +230,49 @@ router.put('/:id', getProduct, async (req, res) => {
   res.product.colors = colors || res.product.colors;
   try {
     await res.product.save();
-    res.json({ message: 'Producto actualizado completamente.', product: res.product });
+    res.json({
+      message: "Producto actualizado completamente.",
+      product: res.product,
+    });
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
 });
 
 // Actualiza una parte de un producto
-router.patch('/:id', getProduct, async (req, res) => {
-  const { brand, model, release_year, price, osname, osversion, dimensions, weight, specifications, connectivity, colors } = req.body;
-  if (!brand && !model && !release_year && !price && !osname && !osversion && !dimensions && !weight && !specifications && !connectivity && !colors) {
-    return res.status(400).json({ message: 'Debe enviar al menos un campo para actualizar.' });
+router.patch("/:id", getProduct, async (req, res) => {
+  const {
+    brand,
+    model,
+    release_year,
+    price,
+    osname,
+    osversion,
+    dimensions,
+    weight,
+    specifications,
+    connectivity,
+    colors,
+  } = req.body;
+  if (
+    !brand &&
+    !model &&
+    !release_year &&
+    !price &&
+    !osname &&
+    !osversion &&
+    !dimensions &&
+    !weight &&
+    !specifications &&
+    !connectivity &&
+    !colors
+  ) {
+    return res
+      .status(400)
+      .json({ message: "Debe enviar al menos un campo para actualizar." });
   }
   res.product.brand = brand || res.product.brand;
-  res.product.model = model || res.product.model; 
+  res.product.model = model || res.product.model;
   res.product.release_year = release_year || res.product.release_year;
   res.product.price = price || res.product.price;
   res.product.osname = osname || res.product.osname;
@@ -180,7 +284,10 @@ router.patch('/:id', getProduct, async (req, res) => {
   res.product.colors = colors || res.product.colors;
   try {
     await res.product.save();
-    res.json({ message: 'Producto actualizado parcialmente.', product: res.product });
+    res.json({
+      message: "Producto actualizado parcialmente.",
+      product: res.product,
+    });
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
